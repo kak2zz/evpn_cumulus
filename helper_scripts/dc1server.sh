9b558050bcf3f4b6ce42cfb8ff94041a0fde23b3
Evgeniy Enin <eugene.enin@gmail.com>
2020-02-26 16:40:35 +0000

    update

--- helper_scripts/dc1server.sh
+++ helper_scripts/dc1server.sh
@@ -1,38 +1,18 @@
 #!/bin/bash
 
 echo "#################################"
-echo "  Configure namespaces.."
+echo "  Configure.."
 echo "#################################"
 
+ip link add link bond0 bond0.100 type vlan proto 802.1ad id 100
+ip link add link bond0.100 bond0.100.20 type vlan proto 802.1Q id 20
+ip link set bond0 up
+ip link set bond0.100 up
+ifconfig bond0.100.20 10.100.20.1/24
 
-#vlan test over vxlan with vlan-aware bridge
-ip netns add vlan_test
-vconfig add bond0 100
-ip link set dev bond0.100 netns vlan_test
-ip netns exec vlan_test ip link set bond0.100 address 02:ac:10:ff:00:13
-ip netns exec vlan_test ip link set bond0.100 up
-ip netns exec vlan_test ip link set bond0.100 mtu 1500
-ip netns exec vlan_test ip addr add 10.0.100.1/24 dev bond0.100
-
-#QinQ test over vxlan with linux bridge
-ip netns add qinq_test_1000_10
-ip netns add qinq_test_1000_20
-vconfig add bond0 1000
-ip link set up bond0.1000 
-vconfig add bond0.1000 10
-vconfig add bond0.1000 20
-ip link set dev bond0.1000.10 netns qinq_test_1000_10
-ip link set dev bond0.1000.20 netns qinq_test_1000_20
-ip netns exec qinq_test_1000_10 ip link set bond0.1000.10 address 02:ac:10:ff:00:14
-ip netns exec qinq_test_1000_20 ip link set bond0.1000.20 address 02:ac:10:ff:00:15
-ip netns exec qinq_test_1000_10 ip link set bond0.1000.10 up
-ip netns exec qinq_test_1000_20 ip link set bond0.1000.20 up
-ip netns exec qinq_test_1000_10 ip link set bond0.1000.10 mtu 1500
-ip netns exec qinq_test_1000_20 ip link set bond0.1000.20 mtu 1500
-ip netns exec qinq_test_1000_10 ip addr add 10.100.10.1/24 dev bond0.1000.10
-ip netns exec qinq_test_1000_20 ip addr add 10.100.20.1/24 dev bond0.1000.20
-
-
+ip link add link bond0 bond0.200 type vlan proto 802.1Q id 200
+ip link set bond0.200 up
+ifconfig bond0.200 10.200.0.1/24
 
 
 echo "#################################"
